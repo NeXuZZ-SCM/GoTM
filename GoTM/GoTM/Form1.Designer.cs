62e70d8fefc7b1b965e30e3fd9c9b9f73d137b87
nexuzz <nexuzz@outlook.com>
2019-01-15 12:29:59 -0300

    Modificaciones Para la version 2 Creacion de Rama V1.1

--- GoTM/GoTM/Form1.Designer.cs
+++ GoTM/GoTM/Form1.Designer.cs
@@ -31,69 +31,85 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.webBrowser1 = new System.Windows.Forms.WebBrowser();
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
-            this.button2 = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.btn_cambiarTraductor = new System.Windows.Forms.Button();
             this.tableLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // webBrowser1
             // 
-            this.tableLayoutPanel1.SetColumnSpan(this.webBrowser1, 2);
+            this.tableLayoutPanel1.SetColumnSpan(this.webBrowser1, 3);
             this.webBrowser1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.webBrowser1.Location = new System.Drawing.Point(3, 3);
             this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
             this.webBrowser1.Name = "webBrowser1";
             this.webBrowser1.ScriptErrorsSuppressed = true;
-            this.webBrowser1.Size = new System.Drawing.Size(485, 417);
+            this.webBrowser1.Size = new System.Drawing.Size(485, 436);
             this.webBrowser1.TabIndex = 0;
             // 
             // tableLayoutPanel1
             // 
-            this.tableLayoutPanel1.ColumnCount = 2;
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
             this.tableLayoutPanel1.Controls.Add(this.webBrowser1, 0, 0);
             this.tableLayoutPanel1.Controls.Add(this.button1, 0, 1);
-            this.tableLayoutPanel1.Controls.Add(this.button2, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.button2, 2, 1);
+            this.tableLayoutPanel1.Controls.Add(this.btn_cambiarTraductor, 1, 1);
             this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
             this.tableLayoutPanel1.RowCount = 2;
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 94F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 6F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(491, 450);
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(491, 471);
             this.tableLayoutPanel1.TabIndex = 1;
-            // 
-            // button2
-            // 
-            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button2.ForeColor = System.Drawing.Color.DodgerBlue;
-            this.button2.Location = new System.Drawing.Point(413, 426);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 21);
-            this.button2.TabIndex = 2;
-            this.button2.Text = "Acerca De";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button1
             // 
             this.button1.Anchor = System.Windows.Forms.AnchorStyles.Left;
             this.button1.BackgroundImage = global::GoTM.Resource1.icons8_pin_filled_50;
             this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
-            this.button1.Location = new System.Drawing.Point(3, 426);
+            this.button1.Location = new System.Drawing.Point(3, 446);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(24, 21);
             this.button1.TabIndex = 1;
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // button2
+            // 
+            this.button2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.ForeColor = System.Drawing.Color.DodgerBlue;
+            this.button2.Location = new System.Drawing.Point(329, 445);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(159, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Acerca De";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // btn_cambiarTraductor
+            // 
+            this.btn_cambiarTraductor.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_cambiarTraductor.ForeColor = System.Drawing.Color.DodgerBlue;
+            this.btn_cambiarTraductor.Location = new System.Drawing.Point(166, 445);
+            this.btn_cambiarTraductor.Name = "btn_cambiarTraductor";
+            this.btn_cambiarTraductor.Size = new System.Drawing.Size(157, 23);
+            this.btn_cambiarTraductor.TabIndex = 3;
+            this.btn_cambiarTraductor.Text = "Cambiar A BingTranslate";
+            this.btn_cambiarTraductor.UseVisualStyleBackColor = true;
+            this.btn_cambiarTraductor.Click += new System.EventHandler(this.btn_cambiarTraductor_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(491, 450);
+            this.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.ClientSize = new System.Drawing.Size(491, 471);
             this.Controls.Add(this.tableLayoutPanel1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
@@ -109,6 +125,7 @@
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button btn_cambiarTraductor;
     }
 }
 
--- GoTM/GoTM/Form1.cs
+++ GoTM/GoTM/Form1.cs
@@ -12,10 +12,11 @@ namespace GoTM
 {
     public partial class Form1 : Form
     {
+        System.Uri Uri = new System.Uri("https://translate.google.com.ar/");
         public Form1()
         {
             InitializeComponent();
-            webBrowser1.Url = new System.Uri("https://translate.google.com.ar/") ;
+            webBrowser1.Url = this.Uri ;
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -36,6 +37,27 @@ namespace GoTM
         {
             AcercaDe ventanaAcerca = new AcercaDe();
             ventanaAcerca.Show();
+        }
+
+        private void btn_cambiarTraductor_Click(object sender, EventArgs e)
+        {
+            
+            if ( this.Uri.Host == "translate.google.com.ar")
+            {
+                this.webBrowser1.Navigate("https://www.bing.com/translator",
+                          null,
+                          null,
+                          "User-Agent:Mozilla/5.0 (Linux; Android 4.2.1; en-us; Nexus 5 Build/JOP40D) AppleWebKit/535.19 (KHTML, like Gecko; googleweblight) Chrome/38.0.1025.166 Mobile Safari/535.19");
+                this.Uri = new System.Uri("https://www.bing.com/translator");
+                btn_cambiarTraductor.Text = "Cambiar a Google Translate";
+            }
+            else
+            {
+                this.Uri = new System.Uri("https://translate.google.com.ar/");
+                btn_cambiarTraductor.Text = "Cambiar a Bing Translate";
+            }
+            webBrowser1.Url = this.Uri;
+            
         }
     }
 }
